3d1e06b7622fbe05b7df1989fc8d03db02989127
Andrea Aguiire <4ndr3420@gmail.com>
2024-08-21 12:56:59 -0600

    Agregando clase app para iniciarlizar la bd cuando se inicie la aplicacion

---
+++ app/src/main/java/com/cursokotlin/misnotas/MisNotasApp.kt
@@ -0,0 +1,23 @@
+package com.cursokotlin.misnotas
+
+import android.app.Application
+import android.arch.persistence.room.Room
+import com.cursokotlin.misnotas.bd.TaskDatabase
+
+//con : Application() Le indicamos a Andorind que esta clase debe instanciarse al inicio de la aplicacion
+//Tambien el nombre de la clase se debe agregar en el manisfest dentro de la etiqueta <Application>
+class MisNotasApp: Application() {
+
+    //Con esta declaracion podemos acceder a la bd desde cualquier lugar de la app
+    companion object{
+        lateinit var database: TaskDatabase
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        //Esta instancia recibe 3 parametros(contexto, la clase donde se encuentra la bd y el nombre que le daremos a la bd)
+        database = Room.databaseBuilder(this, TaskDatabase::class.java, "tasks-db").build()
+    }
+
+}
