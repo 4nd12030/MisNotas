e126c6ce9012bb47d68c210a72d9a50dafaf039a
Andrea Aguiire <4ndr3420@gmail.com>
2024-08-21 20:54:50 -0600

    pendiente por implimentar las corrutinas

--- app/src/main/java/com/cursokotlin/misnotas/MainActivity.kt
+++ app/src/main/java/com/cursokotlin/misnotas/MainActivity.kt
@@ -1,39 +1,112 @@
 package com.cursokotlin.misnotas
 
+import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.cursokotlin.misnotas.adapter.TaskAdapter
 import com.cursokotlin.misnotas.databinding.ActivityMainBinding
 import com.cursokotlin.misnotas.modelo.TaskEntity
+import com.google.android.material.internal.ViewUtils.hideKeyboard
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var bindibg: ActivityMainBinding
-
+    lateinit var binding: ActivityMainBinding
+    lateinit var btnAddTask: Button
+    lateinit var etTask: EditText
     lateinit var recyclerView: RecyclerView
+
     lateinit var tasks: MutableList<TaskEntity>
+    lateinit var adapter: TaskAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bindibg = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bindibg.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        btnAddTask = binding.btnAddTask
+        etTask = binding.etTask
+        recyclerView = binding.rvTask
 
         tasks = ArrayList()
         getTask()
+
+        btnAddTask.setOnClickListener {
+            addTask(TaskEntity(name = etTask.text.toString()))
+        }
+    }
+
+    private fun addTask(task: TaskEntity) = runBlocking {
+        launch {
+            val id = MisNotasApp.database.taskDao().addTask(task)
+            val recoveryTask = MisNotasApp.database.taskDao().getTaskById(id)
+
+            runOnUiThread {
+                tasks.add(recoveryTask)
+                adapter.notifyItemInserted(tasks.size)
+                clearFocus()
+                hideKeyBoard()
+            }
+        }
+
+    }
+
+    private fun clearFocus() {
+        etTask.setText("")
+    }
+
+    private fun hideKeyBoard(){
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
     }
 
     private fun getTask() = runBlocking{
         launch {
             tasks = MisNotasApp.database.taskDao().getAllTasks()
+
             runOnUiThread{
                 setUpRecyclerView(tasks)
             }
         }
     }
 
-    private fun setUpRecyclerView(tasks: MutableList<TaskEntity>) {
+    private fun setUpRecyclerView(tasks: List<TaskEntity>) {
+        adapter = TaskAdapter(
+            tasks,
+            { task -> updateTask(task) },
+            { task -> deleteTask(task)})
+
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+    }
+
+
+    private fun updateTask(task: TaskEntity) = runBlocking{
+        launch {
+            task.isDone = !task.isDone
+            MisNotasApp.database.taskDao().updateTask(task)
+        }
+    }
+
+    private fun deleteTask(task: TaskEntity)= runBlocking{
+        launch {
+            val position = tasks.indexOf(task)
+            MisNotasApp.database.taskDao().deleteTask(task)
+            tasks.remove(task)
+            runOnUiThread {
+                adapter.notifyItemRemoved(position)
+            }
+        }
+
+
 
     }
-}
--- app/src/main/java/com/cursokotlin/misnotas/interfaz/TaskDao.kt
+++ app/src/main/java/com/cursokotlin/misnotas/interfaz/TaskDao.kt
@@ -2,7 +2,11 @@ package com.cursokotlin.misnotas.interfaz
 
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Index
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.cursokotlin.misnotas.modelo.TaskEntity
 
 //Esta interfaz contendra las consultas de la bd
@@ -12,4 +16,17 @@ import com.cursokotlin.misnotas.modelo.TaskEntity
 interface TaskDao {
     @Query("SELECT * FROM task_entity")
     fun getAllTasks() : MutableList<TaskEntity>
+
+    @Insert
+    fun addTask(taskEntity: TaskEntity) : Long
+
+    @Query("SELECT * FROM task_entity WHERE id LIKE  :id")
+    fun getTaskById(id: Long): TaskEntity
+
+    @Update
+    fun updateTask(taskEntity: TaskEntity): Int
+
+    @Delete
+    fun deleteTask(taskEntity: TaskEntity): Int
+
 }
